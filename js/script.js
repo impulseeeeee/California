94793df4cc299140e4da8bef38112258366a2bed
impulseeeeee <chuikonikita2003@gmail.com>
2022-07-18 23:45:15 +0300

    slider(without adaptive)

--- js/script.js
+++ js/script.js
@@ -13,10 +13,38 @@ document.addEventListener('keyup', function (event) {
 
 
 let burgerIcon = document.querySelector('.burger');
+let menu = document.querySelector('.menu')
 
 if (burgerIcon) {
     let navbar = document.querySelector('.navbar');
     burgerIcon.addEventListener("click", function (e) {
         navbar.classList.toggle('active')
+        document.body.classList.toggle("active");
+        menu.classList.toggle("active")
     })
 }
+
+let searchInput = document.querySelector('.input');
+let search = document.querySelector('.search');
+
+search.addEventListener("click", function (ev) {
+    searchInput.classList.toggle("active");
+});
+
+new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    spaceBetween: 100,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+    },
+    autoHeight: true,
+
+})
