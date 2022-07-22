03f3277908c70ffd62537d8eb155b6c6eaea35fe
impulseeeeee <chuikonikita2003@gmail.com>
2022-07-22 22:37:17 +0300

    +pop-up

--- js/script.js
+++ js/script.js
@@ -1,52 +1,109 @@
-let subMenuActivation = document.querySelector('.arrow');
-let subMenu = document.querySelector('.sub-menu');
+//Під-МЕНЮ
+const subMenu = document.querySelector('.sub-menu');
 
-subMenuActivation.addEventListener("click", function () {
-    subMenu.classList.toggle("active");
-});
+document.addEventListener("click", function (sub) {
+    if (sub.target.closest(".arrow")) {
+        subMenu.classList.toggle("active");
+    }
+
+    ;
+
+    if (!sub.target.closest(".activation")) {
+        subMenu.classList.remove("active");
+    }
+}
+
+);
 
 document.addEventListener('keyup', function (event) {
     if (event.code === 'Escape') {
         subMenu.classList.remove("active");
+        popUp.classList.remove('open')
     }
-});
+}
+
+);
 
 
-let burgerIcon = document.querySelector('.burger');
-let menu = document.querySelector('.menu');
+
+//Бургер Меню
+const burgerIcon = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
 
 if (burgerIcon) {
-    let navbar = document.querySelector('.navbar');
+    const navbar = document.querySelector('.navbar');
+
     burgerIcon.addEventListener("click", function () {
         navbar.classList.toggle('active');
         document.body.classList.toggle("active");
         menu.classList.toggle("active");
     });
-};
+}
 
-/*
-let searchInput = document.querySelector('.input');
-let search = document.querySelector('.search');
+;
 
-search.addEventListener("click", () => {
-    searchInput.classList.toggle("active");
-});
-*/
-
+//Слайдер 
 const swiper = new Swiper('.swiper-container', {
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
-    },
+    }
+
+    ,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
-    },
+    }
+
+    ,
     spaceBetween: 100,
     keyboard: {
         enabled: true,
         onlyInViewport: true,
-    },
+    }
+
+    ,
     autoHeight: true,
     variableWidth: true,
+}
+
+);
+
+
+//Пошук за словами
+const words = document.querySelector('.words');
+const pasteBtn = words.querySelectorAll('.btn');
+const searchInput = document.querySelector('#search');
+
+pasteBtn.forEach(element => {
+    element.addEventListener("click", function () {
+        searchInput.value = element.innerHTML;
+    });
+}
+
+);
+
+
+//Pop-Up
+const search = document.querySelector('.search');
+const popUp = document.querySelector('.popup');
+const closePopUp = document.querySelector('.popup__close')
+
+document.addEventListener("click", function (pop) {
+    if (pop.target.closest(".search")) {
+        popUp.classList.add('open')
+        document.body.classList.add("open");
+    };
+
+    if (pop.target.closest(".popup__close")) {
+        popUp.classList.remove('open')
+        document.body.classList.remove("open");
+    };
+
+    popUp.addEventListener("click", function (close) {
+        if (!close.target.closest(".popup__content")) {
+            popUp.classList.remove('open')
+            document.body.classList.remove("open");
+        };
+    });
 });
